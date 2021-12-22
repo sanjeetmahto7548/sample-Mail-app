ff3cd3ca36bb75ac56cc76f8ef17e93cafbdc1f9
sanjeetkumar8285125 <55954257+sanjeetkumar8285125@users.noreply.github.com>
2021-12-22 18:42:41 +0530

    cors used

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+const cors=require('cors')
 
+app.use(cors());
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 
 app.use('/',require('./route/index'))
 app.use('/',require('./route/mail'))
