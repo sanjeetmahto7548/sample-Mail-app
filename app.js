f479ead2104a33de385b448c3f05ee2726b3069c
sanjeetkumar8285125 <55954257+sanjeetkumar8285125@users.noreply.github.com>
2021-12-23 13:09:45 +0530

    cors

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const app=express();
 const cors=require('cors')
-// app.use(cors({
-//     origin:"http://localhost:44322",
-//     credentials: true,
-// }));
+app.use(cors({
+    origin:"*",
+    credentials: true,
+}));
 
 app.use(express.json())
-app.use(cors());
+//app.use(cors());
 app.use(express.urlencoded({extended:true}))
 
 app.use('/',require('./route/mail'))
